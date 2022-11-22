6e3f95de159498de9f398164c00bb3296973b04b
olyapolishchuk <polishchukov2015@gmail.co>
2022-11-22 13:43:44 +0200

    Initial commit

---
+++ src/main/java/com/portfolio/coronavirustrack/CoronavirusTrackApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.coronavirustrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoronavirusTrackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoronavirusTrackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/portfolio/coronavirustrack/CoronavirusTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.coronavirustrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronavirusTrackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
